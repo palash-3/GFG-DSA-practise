d6e57e0aca6f3b74ecec3428973b7a4bf0cb359b
Palash Chaudhari <chaudharipalash50@gmail.com>
2026-05-22 20:42:56 -0700

    array practise 22 may

--- Array-List/MoveZeroAtLast-BestApproach.py
+++ Array-List/MoveZeroAtLast-BestApproach.py
@@ -6,12 +6,8 @@ def MoveZero(arr):
 
     for i in range(n):
         if arr[i] != 0:
-            arr[count] = arr[i]
+            arr[count], arr[i] = arr[i], arr[count]
             count+=1
-            
-    while count < n:
-        arr[count] = 0
-        count+=1
     
     print(arr)
 
