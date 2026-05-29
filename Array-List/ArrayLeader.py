9af6cf72d07cfee32f7440f43116e56c7c6cce5a
Palash Chaudhari <chaudharipalash50@gmail.com>
2026-05-28 18:32:15 -0700

    array practise 28 may

--- Array-List/ArrayLeader.py
+++ Array-List/ArrayLeader.py
@@ -2,15 +2,19 @@ arr = [16, 17, 4, 3, 5, 2]
 
 def array_leader(arr):
     n = len(arr)
-    temp = []
+    temp_arr = []
 
     for i in range(n):
-        for j in range(n):
+        flag = 0
+        for j in range(i+1,n):
             if arr[i] < arr[j]:
+                flag+=1
                 break
-            else:
-                temp.append(arr[i])
-    return temp
+        if flag == 0:
+            temp_arr.append(arr[i])
+                
+    return temp_arr
+
 
 leader = array_leader(arr)
 print(leader)
