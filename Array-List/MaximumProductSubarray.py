70b555c3a504a06a4ed1bd107c65bb64b06e145d
Palash Chaudhari <chaudharipalash50@gmail.com>
2026-06-09 07:29:13 -0700

    array practise 9 june

--- Array-List/MaximumProductSubarray.py
+++ Array-List/MaximumProductSubarray.py
@@ -1,16 +1,17 @@
-arr =[-2, 6, -3, -10, 0, 2]
+#arr = [-2, 6, -3, -10, 0, 2]
+#arr = [-1, -3, -10, 0, 6]
+arr = [2, 3, 4] 
 
-def max_product(arr):
+def max_aubarray_product(arr):
     n = len(arr)
-    maxProd = arr[0]
+    max_product = arr[0]
 
-    for i in range(n):
-        mul = 1
-        for j in range(i, n):
-            mul *= arr[j]
-            maxProd = max(maxProd, mul)
-    
-    return maxProd
+    for i in range(n-1):
+        curr_product = 1
+        for j in range(i,n):
+            curr_product = curr_product*arr[j]
+            max_product = max(max_product, curr_product)
+    return max_product
 
-product = max_product(arr)
+product = max_aubarray_product(arr)
 print(product)
