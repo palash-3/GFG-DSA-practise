6d5789e5186dc562b48ba4c7dfa6c1468bb86883
Palash Chaudhari <chaudharipalash50@gmail.com>
2026-06-11 20:18:52 -0700

    search practise 11 june

--- Search/BinarySearch.py
+++ Search/BinarySearch.py
@@ -1,20 +1,44 @@
-#Binary Search using IF
+# #Binary Search using IF
+
+# arr = [1,2,3,4,5,6,7]
+# key = 7
+
+# def binary_search(arr, key, low, high):
+#     if high >= low:
+#         mid = low+(high-low)//2
+
+#         if arr[mid] == key:
+#             return mid
+#         elif arr[mid] > key:
+#             return binary_search(arr, key, low, mid-1)
+#         else:
+#             return binary_search(arr, key, mid+1, high)
+#     else:
+#         return -1
+    
+# index = binary_search(arr, key, 0, len(arr)-1)
+# print(index)
+
+
+#Binary Search using While loop
 
 arr = [1,2,3,4,5,6,7]
 key = 7
 
-def binary_search(arr, key, low, high):
-    if high >= low:
+def binary_search(arr, key):
+    low = 0
+    high = len(arr)-1
+
+    while low <= high:
         mid = low+(high-low)//2
 
         if arr[mid] == key:
             return mid
         elif arr[mid] > key:
-            return binary_search(arr, key, low, mid-1)
+            high = mid-1
         else:
-            return binary_search(arr, key, mid+1, high)
-    else:
-        return -1
-    
-index = binary_search(arr, key, 0, len(arr)-1)
+            low = mid+1
+    return -1
+
+index = binary_search(arr, key)
 print(index)
