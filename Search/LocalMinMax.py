b0e8517f2340d375a3e4ad00020f3a09ed81c951
Palash Chaudhari <chaudharipalash50@gmail.com>
2026-06-15 21:42:49 -0700

    search practise 15 june

---
+++ Search/LocalMinMax.py
@@ -0,0 +1,21 @@
+arr = [6, 4, 2, -2, 5, 3, 2, 2, -1, -1, 4]
+
+def local_min_max(arr):
+    flag = None
+    n = len(arr)
+    temp =[]
+    temp.append(arr[0])
+
+    for i in range(1, n-1):
+        if arr[i] > temp[-1] and arr[i] > arr[i+1]:
+            temp.append(arr[i])
+        elif temp[-1] > arr[i] and arr[i] < arr[i + 1]:
+            temp.append(arr[i])
+    
+    if temp[-1] != arr[-1]:
+        temp.append(arr[-1])
+
+    return temp
+
+result = local_min_max(arr)
+print(result)
