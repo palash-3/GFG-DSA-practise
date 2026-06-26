f312dc1c233197c1eef962877d7135e4a7b92c29
Palash Chaudhari <chaudharipalash50@gmail.com>
2026-06-25 23:33:42 -0700

    25 june

--- 2Pointers/3Sum.py
+++ 2Pointers/3Sum.py
@@ -1,12 +1,12 @@
+arr = [1, 4, 45, 6, 10, 8]
+target = 13
+
 def hasTripletSum(arr, target):
     n = len(arr)
     arr.sort()
     
     # Fix the first element as arr[i]
     for i in range(n - 2):
-        
-        # Initialize left and right pointers with 
-        # start and end of remaining subarray
         l = i + 1
         r = n - 1
         
@@ -21,10 +21,4 @@ def hasTripletSum(arr, target):
     
     return False
 
-if __name__ == "__main__":
-    arr = [1, 4, 45, 6, 10, 8]
-    target = 13
-    if hasTripletSum(arr, target):
-        print("true")
-    else:
-        print("false")
+print(hasTripletSum(arr, target))
