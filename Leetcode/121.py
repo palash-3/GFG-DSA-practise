558710c6ec9e403a4b6082b2ddeabbc7fe89eff3
Palash Chaudhari <chaudharipalash50@gmail.com>
2026-07-26 23:39:07 -0700

    leetcode

---
+++ Leetcode/121.py
@@ -0,0 +1,14 @@
+prices = [7,1,5,3,6,4]
+
+def maxProfit(prices) -> int:
+    min_price = prices[0]
+    max_profit = 0
+
+    for price in prices[1:]:
+        max_profit = max(max_profit, price - min_price)
+        min_price = min(min_price, price)
+
+    return max_profit
+
+result = maxProfit(prices)
+print(result)
