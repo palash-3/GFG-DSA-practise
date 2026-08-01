21d43ff7a185c3d0ae0c7c522582ede763be7682
Palash Chaudhari <chaudharipalash50@gmail.com>
2026-08-01 14:49:11 -0700

    leetcode

--- Leetcode/80.py
+++ Leetcode/80.py
@@ -1,14 +1,14 @@
 nums = [1,1,1,2,2,3]
 
-def removeDuplicates(nums): 
+def remove_duplicate(nums):
     write_pos = 2
 
     for i in range(2, len(nums)):
         if nums[i] != nums[write_pos - 2]:
             nums[write_pos] = nums[i]
             write_pos += 1
-
     return write_pos
-
-result = removeDuplicates(nums)
+result = remove_duplicate(nums)
 print(result)
+
+
